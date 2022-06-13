755b4a3ba25b672c1ad3d058d125062c427ac3f2
marcelodevfullstack <marcelosoares.br@gmail.com>
2022-06-13 20:32:24 -0300

    Repositories implementados

---
+++ src/main/java/com/marcelo/bookstore/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.marcelo.bookstore.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.marcelo.bookstore.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
